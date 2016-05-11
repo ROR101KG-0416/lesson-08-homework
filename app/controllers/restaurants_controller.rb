a3cc5e874c24b1d6925039c871a1e0c13206265f
Gabriela Caires <gabicairesj@gmail.com>
2016-05-11 12:00:24 -0400

    added new, edit and destroy actions

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,23 +9,38 @@ class RestaurantsController < ApplicationController
   end
 
   def new
-
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
 
+    @restaurant.save
+
+    redirect_to restaurants_path
   end
 
   def edit
-
+    @restaurant = Restaurant.find_by_id(params[:id])
   end
 
   def update
+    @restaurant = Restaurant.find_by_id(params[:id])
 
+    @restaurant.update(restaurant_params)
+
+    redirect_to restaurant_path(restaurant_params)
   end
 
   def destroy
+    @restaurant = Restaurant.find_by_id(params[:id])
 
+    @restaurant.delete
+
+    redirect_to restaurants_path
   end  
 
+  def restaurant_params 
+    params.require(:restaurant).permit(:name, :street, :city, :state, :country, :postal_code)
+  end  
 end  
